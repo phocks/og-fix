7f627f391f732e99400a8d0ad1a42c3b622f9372
Joshua Byrd <phocks@gmail.com>
2024-04-05 08:33:48 +1000

    This should work???

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
+// module.exports = {
+//   reactStrictMode: true,
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     config.experiments = {
+//       asyncWebAssembly: true,
+//       layers: true,
+//     };
+//     return config;
+//   },
+// };
