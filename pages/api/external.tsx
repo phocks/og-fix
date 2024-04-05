8a8d100a8f92ff06978dfa9f9b5b91e84849943f
Joshua Byrd <phocks@gmail.com>
2024-04-06 09:48:35 +1000

    Proof on concept

--- pages/api/external.tsx
+++ pages/api/external.tsx
@@ -34,9 +34,9 @@ export default async function handler(request: NextRequest) {
         <img
           width="1200"
           height="630"
-          src={`https://i.guim.co.uk/img/media/2caa54def70d6082b5a48a44084d9bf73d8d706a/0_120_2880_1728/master/2880.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=773ee5b3dc11b6f4582dff953eba5465`}
+          src={url}
           style={{
-            borderRadius: 128,
+            
           }}
         />
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,67 @@
-import Head from 'next/head'
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import url from "url";
+import http from "http";
+import https from "https";
+import { load } from "cheerio";
 
-export default function Page() {
+export default function Page({ ogImage, ogTitle, ogDescription, targetUrl }) {
   return (
     <div>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
+        <meta name="og:title" content={ogTitle || "Vercel Edge Network"} />
+        <meta
+          name="og:description"
+          content={ogDescription || "Vercel Edge Network"}
+        />
         <meta
           name="og:image"
-          content={ 
-            `https://ogto.vercel.app/api/external?url=https://www.theguardian.com/world/2024/apr/05/israeli-inquiry-blames-wck-aid-killings-on-grave-errors-by-military-personnel`
-          }
+          content={`https://ogto.vercel.app/api/external?url=${ogImage}`}
         />
+        Redirect immediately to the target URL
+        <meta httpEquiv="refresh" content={`0;url=${targetUrl}`} />
       </Head>
-      <h1>Open graph fix.</h1>
+      <h1>...</h1>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const targetUrl = context.query.url as string;
+
+  if (!targetUrl) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const parsedUrl = url.parse(targetUrl);
+  const getter = parsedUrl.protocol === "https:" ? https.get : http.get;
+
+  const data = await new Promise<string>((resolve, reject) => {
+    getter(targetUrl, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => resolve(data));
+      res.on("error", reject);
+    });
+  });
+
+  const $ = load(data);
+  const ogImage = $('meta[property="og:image"]').attr("content");
+  const ogTitle = $('meta[property="og:title"]').attr("content");
+  const ogDescription = $('meta[property="og:description"]').attr("content");
+
+  return {
+    props: {
+      ogImage,
+      ogTitle,
+      ogDescription,
+      targetUrl,
+    },
+  };
+};
